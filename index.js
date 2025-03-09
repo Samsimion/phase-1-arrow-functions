9b7bf8fa9b13b8a1d75023eb3fa474d44f389a7a
simion samuel <samuel.simion@student.moringaschool.com>
2025-03-09 11:50:47 +0300

    assign arrow func

---
+++ index.js
@@ -0,0 +1,29 @@
+const divide = (num1,num2) => {
+    return num1/num2;
+
+}
+
+const square = (num3)  => {
+    return num3 ** 2;
+}
+
+const add =(num4, num5) => {
+    return num4 + num5;
+}
+
+
+/*it("square arrow function takes one parameter and multiplies it times itself", () => {
+    let x = 2
+    expect(square(x)).to.equal(4)
+  })
+
+  it("has an arrow function called add", () => {
+    expect(add).to.exist
+  })
+
+  it("add arrow function takes two parameters and adds them together", () => {
+    let a = 3
+    let b = 4
+    expect(add(a,b)).to.equal(a+b)
+  })
+})*/
